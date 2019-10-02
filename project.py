9a8eb579d9a168eb986fb3fea473f3f67253673c
Ana Nogueira <ana.nogueira.mu@gmail.com>
2019-10-02 16:27:36 +0100

    add new graph

--- project.py
+++ project.py
@@ -6,8 +6,9 @@ import pandas as pd
 from pandas.plotting import register_matplotlib_converters
 import matplotlib.pyplot as plt
 import functions as func
-
-
+import seaborn as sns
+import scipy.stats as _stats
+ 
 register_matplotlib_converters()
 data = pd.read_csv('data/pd.csv',  index_col = 'id', header = 1)
 
@@ -74,7 +75,55 @@ for i in range(len(columns)):
         axs[i, j].set_ylabel("probability")
         axs[i, j].hist(data[columns[i]].dropna().values, bins[j])
 fig.tight_layout()
+#plt.show()
+
+
+
+import scipy.stats as _stats 
+def compute_known_distributions(x_values, n_bins) -> dict:
+    distributions = dict()
+    # Gaussian
+    mean, sigma = _stats.norm.fit(x_values)
+    distributions['Normal(%.1f,%.2f)'%(mean,sigma)] = _stats.norm.pdf(x_values, mean, sigma)
+    # LogNorm
+  #  sigma, loc, scale = _stats.lognorm.fit(x_values)
+  #  distributions['LogNor(%.1f,%.2f)'%(np.log(scale),sigma)] = _stats.lognorm.pdf(x_values, sigma, loc, scale)
+    # Exponential
+    loc, scale = _stats.expon.fit(x_values)
+    distributions['Exp(%.2f)'%(1/scale)] = _stats.expon.pdf(x_values, loc, scale)
+    # SkewNorm
+   # a, loc, scale = _stats.skewnorm.fit(x_values)
+   # distributions['SkewNorm(%.2f)'%a] = _stats.skewnorm.pdf(x_values, a, loc, scale) 
+    return distributions
+
+def histogram_with_distributions(ax: plt.Axes, series: pd.Series, var: str):
+    values = series.sort_values().values
+    n, bins, patches = ax.hist(values, 20, density=True, edgecolor='grey')
+    distributions = compute_known_distributions(values, bins)
+    func.multiple_line_chart(ax, values, distributions, 'Best fit for %s'%var, var, 'probability')
+
+columns = new_data.select_dtypes(include='number').columns
+rows, cols = func.choose_grid(len(columns))
+plt.figure()
+fig, axs = plt.subplots(rows, cols, figsize=(cols*4, rows*4), squeeze=False)
+i, j = 0, 0
+for n in range(len(columns)):
+    histogram_with_distributions(axs[i, j], new_data[columns[n]].dropna(), columns[n])
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.tight_layout()
 plt.show()
 
 
 
+columns = new_data.select_dtypes(include='number').columns
+rows, cols = func.choose_grid(len(columns))
+plt.figure()
+fig, axs = plt.subplots(rows, cols, figsize=(cols*4, rows*4), squeeze=False)
+i, j = 0, 0
+for n in range(len(columns)):
+    axs[i, j].set_title('Histogram with trend for %s'%columns[n])
+    axs[i, j].set_ylabel("probability")
+    sns.distplot(data[columns[n]].dropna().values, norm_hist=True, ax=axs[i, j], axlabel=columns[n])
+    i, j = (i + 1, 0) if (n+1) % cols == 0 else (i, j + 1)
+fig.tight_layout()
+plt.show()
