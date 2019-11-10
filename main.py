e94bf7fa16d48a2b9361ff2210cb969d59d0409d
vilmaneves <vilmacarina.neves@gmail.com>
2019-11-10 16:45:45 +0000

    graficos

--- main.py
+++ main.py
@@ -35,6 +35,10 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score, adjusted_rand_score
 import time, warnings, numpy as np
 import matplotlib.pyplot as plt
+from sklearn.cluster import MiniBatchKMeans, KMeans
+from sklearn.metrics.pairwise import pairwise_distances_argmin
+from sklearn.datasets.samples_generator import make_blobs
+
 
 """ MAIN """
 register_matplotlib_converters()
@@ -135,6 +139,21 @@ def select_Kbest(X, k):
 
     return X_KBest_df
 
+""""def cut_qcut_compare():
+    X_k10_best_cut = X_k10_best_df.copy()
+    X_k10_best_qcut = X_k10_best_df.copy()
+    bins = 2
+    labels = ['0', '1']
+    max_bins = 10
+
+    for i in range(2, max_bins):
+        for col in X_k10_best_cut:
+            X_k10_best_cut[col] = pd.cut(X_k10_best_cut[col], bins, labels=labels)
+            X_k10_best_qcut[col] = pd.cut(X_k10_best_qcut[col], bins, labels=labels)
+            bins += 1
+            labels
+"""
+
 #export1 = X_KBest_df.to_csv(r'x_Kbest.csv', index=None, header=True)
 
 X_k10_best_df = select_Kbest(X, 10)
@@ -143,6 +162,11 @@ X_k10_best_df = select_Kbest(X, 10)
 X_KBest_copy = X_k10_best_df.copy()
 for col in X_KBest_copy:
     X_KBest_copy[col] = pd.cut(X_KBest_copy[col], 3, labels=['0','1','2'])
+
+
+newdf = X_k10_best_df.copy()
+for col in newdf:
+    newdf[col] = pd.qcut(newdf[col],3,labels=['0','1','2'])
 
 #export2 = X_KBest_copy.to_csv(r'x_Kbest_copy.csv', index=None, header=True)
 
@@ -193,7 +217,7 @@ def kmeans_NrClusters_inertia(X):
     plt.ylabel("Number of Inertia")
 
     plt.plot(nr_clusters_list, list_inertia_values)
-    plt.show()
+    #plt.show()
     
     
 def k_means_sillhoutte(X, nr_cluster):
@@ -214,13 +238,78 @@ k_means_sillhoutte(X_normalized,6)
 k_means_adjusted_rand_score(y, 6)
 
 
-k2_best = select_Kbest(X_normalized,2)
-print(k2_best)
+X_k2_best_df = select_Kbest(X_normalized,2)
+#print(X_k2_best_df)
+
+X_k2_best: np.ndarray = X_k2_best_df.values
+#print(X_k2_best)
+
+def clusters_plot(X_k2_best):
+    # 1b compute clustering with Means
+    k_means = KMeans(init='k-means++', n_clusters=6, n_init=10)
+    t0 = time.time()
+    k_means.fit(X_k2_best)
+    t_batch = time.time() - t0
+
+    # 1c compute clustering with MiniBatchKMeans
+    mbk = MiniBatchKMeans(init='k-means++', n_clusters=3, batch_size=45, n_init=10, max_no_improvement=10, verbose=0)
+    t0 = time.time()
+    mbk.fit(X_k2_best)
+    t_mini_batch = time.time() - t0
 
 
+    # 2 plotting differences result
+    fig = plt.figure(figsize=(8, 3))
+    fig.subplots_adjust(left=0.02, right=0.98, bottom=0.05, top=0.9)
+    colors = ['#EB507F', '#137DD2', '#F6E051', '#4EACC5', '#FF9C34', '#4E9A06']
 
-#print("Calinski Harabaz:",metrics.calinski_harabasz_score(X, y_pred))
-#print("Davies Bouldin:",metrics.davies_bouldin_score(X, y_pred))
-#print("Silhouette per instance:",metrics.silhouette_samples(X, y_pred))
+    k_means_cluster_centers = np.sort(k_means.cluster_centers_, axis=0)
+    mbk_means_cluster_centers = np.sort(mbk.cluster_centers_, axis=0)
+    k_means_labels = pairwise_distances_argmin(X_k2_best, k_means_cluster_centers)
+    mbk_means_labels = pairwise_distances_argmin(X_k2_best, mbk_means_cluster_centers)
+    order = pairwise_distances_argmin(k_means_cluster_centers, mbk_means_cluster_centers)
 
 
+    # 2a KMeans
+    ax = fig.add_subplot(1, 3, 1)
+    n_clusters = 6
+    for k, col in zip(range(n_clusters), colors):
+        my_members = k_means_labels == k
+        cluster_center = k_means_cluster_centers[k]
+        ax.plot(X_k2_best[my_members,0], X_k2_best[my_members,1],  'w', markerfacecolor=col, marker='o')
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=6)
+    ax.set_title('KMeans')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    plt.text(-3.5, 1.8,'train time: %.2fs\ninertia: %f' % (t_batch, k_means.inertia_))
+
+
+    # 2b MiniBatchKMeans
+    ax = fig.add_subplot(1, 3, 2)
+    for k, col in zip(range(n_clusters), colors):
+        my_members = mbk_means_labels == order[k]
+        cluster_center = mbk_means_cluster_centers[order[k]]
+        ax.plot(X_k2_best[my_members,0], X_k2_best[my_members,1],'w',markerfacecolor=col,marker='.')
+        ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col, markeredgecolor='k', markersize=6)
+    ax.set_title('MiniBatchKMeans')
+    ax.set_xticks(())
+    ax.set_yticks(())
+    plt.text(-3.5,1.8,'train time: %.2fs\ninertia: %f' % (t_mini_batch, mbk.inertia_))
+
+    # 2c difference between solutions
+    different = (mbk_means_labels == 4)
+    ax = fig.add_subplot(1, 3, 3)
+    for k in range(n_clusters):
+        different += ((k_means_labels == k) != (mbk_means_labels == order[k]))
+
+    identic = np.logical_not(different)
+    ax.plot(X_k2_best[identic, 0], X_k2_best[identic, 1], 'w',markerfacecolor='#bbbbbb', marker='.')
+    ax.plot(X_k2_best[different, 0], X_k2_best[different, 1], 'w', markerfacecolor='m', marker='.')
+    ax.set_title('Difference')
+    ax.set_xticks(())
+    ax.set_yticks(())
+
+    plt.show()
+
+
+clusters_plot(X_k2_best)
